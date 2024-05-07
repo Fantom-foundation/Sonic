11893438471e5319f3814408ddb6df63ff1a7b0c
JK <jk@fantom.foundation>
2024-05-07 14:07:05 +0200

    Rename the version to 1.0.0-a

--- version/version.go
+++ version/version.go
@@ -8,10 +8,10 @@ import (
 )
 
 func init() {
-	params.VersionMajor = 0     // Major version component of the current release
-	params.VersionMinor = 1     // Minor version component of the current release
-	params.VersionPatch = 0     // Patch version component of the current release
-	params.VersionMeta = "dev"  // Version metadata to append to the version string
+	params.VersionMajor = 1  // Major version component of the current release
+	params.VersionMinor = 0  // Minor version component of the current release
+	params.VersionPatch = 0  // Patch version component of the current release
+	params.VersionMeta = "a" // Version metadata to append to the version string
 }
 
 func BigToString(b *big.Int) string {
