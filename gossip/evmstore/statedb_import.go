93c4a2a35af483ab903e0068049c66c95b8338ab
JK <jk@fantom.foundation>
2024-05-29 09:01:36 +0200

    Port legacy genesis import

--- gossip/evmstore/statedb_import.go
+++ gossip/evmstore/statedb_import.go
@@ -1,12 +1,23 @@
 package evmstore
 
 import (
+	"bytes"
 	"fmt"
+	cc "github.com/Fantom-foundation/Carmen/go/common"
 	io2 "github.com/Fantom-foundation/Carmen/go/database/mpt/io"
 	carmen "github.com/Fantom-foundation/Carmen/go/state"
 	"github.com/Fantom-foundation/go-opera/opera/genesis"
+	"github.com/Fantom-foundation/go-opera/utils/adapters/kvdb2ethdb"
+	"github.com/Fantom-foundation/lachesis-base/kvdb/nokeyiserr"
+	"github.com/Fantom-foundation/lachesis-base/kvdb/pebble"
+	"github.com/Fantom-foundation/lachesis-base/kvdb/table"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rlp"
+	"github.com/ethereum/go-ethereum/trie"
+	"github.com/ethereum/go-ethereum/triedb"
 	"io"
 	"os"
 	"path/filepath"
@@ -86,137 +97,139 @@ func (s *Store) ExportArchiveWorldState(out io.Writer) error {
 }
 
 func (s *Store) ImportLegacyEvmData(evmItems genesis.EvmItems, blockNum uint64, root common.Hash) error {
-	panic("legacy import not yet ported to go-ethereum v1.13.14")
-	/*
-		if err := s.Open(); err != nil {
-			return fmt.Errorf("failed to open EvmStore for legacy EVM data import; %v", err)
-		}
-		defer s.Close()
+	if err := s.Open(); err != nil {
+		return fmt.Errorf("failed to open EvmStore for legacy EVM data import; %v", err)
+	}
+	defer s.Close()
 
-		carmenDir, err := os.MkdirTemp(s.parameters.Directory, "opera-tmp-import-legacy-genesis")
+	carmenDir, err := os.MkdirTemp(s.parameters.Directory, "opera-tmp-import-legacy-genesis")
+	if err != nil {
+		panic(fmt.Errorf("failed to create temporary dir for legacy EVM data import: %v", err))
+	}
+	defer os.RemoveAll(carmenDir)
+
+	s.Log.Info("Unpacking legacy EVM data into a temporary directory", "dir", carmenDir)
+	db, err := pebble.New(carmenDir, 1024, 100, nil, nil)
+	if err != nil {
+		panic(fmt.Errorf("failed to open temporary database for legacy EVM data import: %v", err))
+	}
+	evmItems.ForEach(func(key, value []byte) bool {
+		err := db.Put(key, value)
 		if err != nil {
-			panic(fmt.Errorf("failed to create temporary dir for legacy EVM data import: %v", err))
+			return false
 		}
-		defer os.RemoveAll(carmenDir)
+		return true
+	})
 
-		s.Log.Info("Unpacking legacy EVM data into a temporary directory", "dir", carmenDir)
-		db, err := pebble.New(carmenDir, 1024, 100, nil, nil)
-		if err != nil {
-			panic(fmt.Errorf("failed to open temporary database for legacy EVM data import: %v", err))
-		}
-		evmItems.ForEach(func(key, value []byte) bool {
-			err := db.Put(key, value)
-			if err != nil {
-				return false
-			}
-			return true
-		})
+	s.Log.Info("Importing legacy EVM data into Carmen", "index", blockNum, "root", root)
 
-		s.Log.Info("Importing legacy EVM data into Carmen", "index", blockNum, "root", root)
+	var currentBlock uint64 = 1
+	var accountsCount, slotsCount uint64 = 0, 0
+	bulk := s.liveStateDb.StartBulkLoad(currentBlock)
 
-		var currentBlock uint64 = 1
-		var accountsCount, slotsCount uint64 = 0, 0
-		bulk := s.liveStateDb.StartBulkLoad(currentBlock)
-
-		restartBulkIfNeeded := func () error {
-			if (accountsCount + slotsCount) % 1_000_000 == 0 && currentBlock < blockNum {
-				if err := bulk.Close(); err != nil {
-					return err
-				}
-				currentBlock++
-				bulk = s.liveStateDb.StartBulkLoad(currentBlock)
-			}
-			return nil
-		}
-
-		chaindb := rawdb.NewDatabase(kvdb2ethdb.Wrap(nokeyiserr.Wrap(db)))
-		triedb := trie.NewDatabase(chaindb)
-		t, err := trie.NewSecure(root, triedb)
-		if err != nil {
-			return fmt.Errorf("failed to open trie; %v", err)
-		}
-		preimages := table.New(db, []byte("secure-key-"))
-
-		accIter := t.NodeIterator(nil)
-		for accIter.Next(true) {
-			if accIter.Leaf() {
-
-				addressBytes, err := preimages.Get(accIter.LeafKey())
-				if err != nil || addressBytes == nil {
-					return fmt.Errorf("missing preimage for account address hash %v; %v", accIter.LeafKey(), err)
-				}
-				address := cc.Address(common.BytesToAddress(addressBytes))
-
-				var acc state.Account
-				if err := rlp.DecodeBytes(accIter.LeafBlob(), &acc); err != nil {
-					return fmt.Errorf("invalid account encountered during traversal; %v", err)
-				}
-
-				bulk.CreateAccount(address)
-				bulk.SetNonce(address, acc.Nonce)
-				bulk.SetBalance(address, acc.Balance)
-
-
-				if !bytes.Equal(acc.CodeHash, emptyCodeHash) {
-					code := rawdb.ReadCode(chaindb, common.BytesToHash(acc.CodeHash))
-					if len(code) == 0 {
-						return fmt.Errorf("missing code for account %v", address)
-					}
-					bulk.SetCode(address, code)
-				}
-
-				if acc.Root != types.EmptyRootHash {
-					storageTrie, err := trie.NewSecure(acc.Root, triedb)
-					if err != nil {
-						return fmt.Errorf("failed to open storage trie for account %v; %v", address, err)
-					}
-					storageIt := storageTrie.NodeIterator(nil)
-					for storageIt.Next(true) {
-						if storageIt.Leaf() {
-							keyBytes, err := preimages.Get(storageIt.LeafKey())
-							if err != nil || keyBytes == nil {
-								return fmt.Errorf("missing preimage for storage key hash %v; %v", storageIt.LeafKey(), err)
-							}
-							key := cc.Key(common.BytesToHash(keyBytes))
-
-							_, valueBytes, _, err := rlp.Split(storageIt.LeafBlob())
-							if err != nil {
-								return fmt.Errorf("failed to decode storage; %v", err)
-							}
-							value := cc.Value(common.BytesToHash(valueBytes))
-
-							bulk.SetState(address, key, value)
-							slotsCount++
-							if err := restartBulkIfNeeded(); err != nil {
-								return err
-							}
-						}
-					}
-					if storageIt.Error() != nil {
-						return fmt.Errorf("failed to iterate storage trie of account %v; %v", address, storageIt.Error())
-					}
-				}
-
-				accountsCount++
-				if err := restartBulkIfNeeded(); err != nil {
-					return err
-				}
-			}
-		}
-		if accIter.Error() != nil {
-			return fmt.Errorf("failed to iterate accounts trie; %v", accIter.Error())
-		}
-
-		if err := bulk.Close(); err != nil {
-			return err
-		}
-		// add the empty genesis block into archive
-		if currentBlock < blockNum {
-			bulk = s.liveStateDb.StartBulkLoad(blockNum)
+	restartBulkIfNeeded := func() error {
+		if (accountsCount+slotsCount)%1_000_000 == 0 && currentBlock < blockNum {
 			if err := bulk.Close(); err != nil {
 				return err
 			}
+			currentBlock++
+			bulk = s.liveStateDb.StartBulkLoad(currentBlock)
 		}
 		return nil
-	*/
+	}
+
+	chaindb := rawdb.NewDatabase(kvdb2ethdb.Wrap(nokeyiserr.Wrap(db)))
+	tdb := triedb.NewDatabase(chaindb, &triedb.Config{Preimages: false, IsVerkle: false})
+	t, err := trie.NewStateTrie(trie.StateTrieID(root), tdb)
+	if err != nil {
+		return fmt.Errorf("failed to open trie; %v", err)
+	}
+	preimages := table.New(db, []byte("secure-key-"))
+
+	accIter, err := t.NodeIterator(nil)
+	if err != nil {
+		return fmt.Errorf("failed to open accounts iterator; %v", err)
+	}
+	for accIter.Next(true) {
+		if accIter.Leaf() {
+
+			addressBytes, err := preimages.Get(accIter.LeafKey())
+			if err != nil || addressBytes == nil {
+				return fmt.Errorf("missing preimage for account address hash %v; %v", accIter.LeafKey(), err)
+			}
+			address := cc.Address(common.BytesToAddress(addressBytes))
+
+			var acc types.StateAccount
+			if err := rlp.DecodeBytes(accIter.LeafBlob(), &acc); err != nil {
+				return fmt.Errorf("invalid account encountered during traversal; %v", err)
+			}
+
+			bulk.CreateAccount(address)
+			bulk.SetNonce(address, acc.Nonce)
+			bulk.SetBalance(address, acc.Balance.ToBig())
+
+			if !bytes.Equal(acc.CodeHash, emptyCodeHash) {
+				code := rawdb.ReadCode(chaindb, common.BytesToHash(acc.CodeHash))
+				if len(code) == 0 {
+					return fmt.Errorf("missing code for account %v", address)
+				}
+				bulk.SetCode(address, code)
+			}
+
+			if acc.Root != types.EmptyRootHash {
+				storageTrie, err := trie.NewStateTrie(trie.StateTrieID(acc.Root), tdb)
+				if err != nil {
+					return fmt.Errorf("failed to open storage trie for account %v; %v", address, err)
+				}
+				storageIt, err := storageTrie.NodeIterator(nil)
+				if err != nil {
+					return fmt.Errorf("failed to open storage iterator for account %v; %v", address, err)
+				}
+				for storageIt.Next(true) {
+					if storageIt.Leaf() {
+						keyBytes, err := preimages.Get(storageIt.LeafKey())
+						if err != nil || keyBytes == nil {
+							return fmt.Errorf("missing preimage for storage key hash %v; %v", storageIt.LeafKey(), err)
+						}
+						key := cc.Key(common.BytesToHash(keyBytes))
+
+						_, valueBytes, _, err := rlp.Split(storageIt.LeafBlob())
+						if err != nil {
+							return fmt.Errorf("failed to decode storage; %v", err)
+						}
+						value := cc.Value(common.BytesToHash(valueBytes))
+
+						bulk.SetState(address, key, value)
+						slotsCount++
+						if err := restartBulkIfNeeded(); err != nil {
+							return err
+						}
+					}
+				}
+				if storageIt.Error() != nil {
+					return fmt.Errorf("failed to iterate storage trie of account %v; %v", address, storageIt.Error())
+				}
+			}
+
+			accountsCount++
+			if err := restartBulkIfNeeded(); err != nil {
+				return err
+			}
+		}
+	}
+	if accIter.Error() != nil {
+		return fmt.Errorf("failed to iterate accounts trie; %v", accIter.Error())
+	}
+
+	if err := bulk.Close(); err != nil {
+		return err
+	}
+	// add the empty genesis block into archive
+	if currentBlock < blockNum {
+		bulk = s.liveStateDb.StartBulkLoad(blockNum)
+		if err := bulk.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
 }
