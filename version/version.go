ae0662378f405c47994708ebaaeabb92cee432a4
JK <jk@fantom.foundation>
2024-06-10 10:32:11 +0200

    Tag Sonic-1.2.1-c

--- version/version.go
+++ version/version.go
@@ -9,7 +9,7 @@ func init() {
 	params.VersionMajor = 1  // Major version component of the current release
 	params.VersionMinor = 2  // Minor version component of the current release
 	params.VersionPatch = 1  // Patch version component of the current release
-	params.VersionMeta = "b" // Version metadata to append to the version string
+	params.VersionMeta = "c" // Version metadata to append to the version string
 }
 
 func AsString() string {
