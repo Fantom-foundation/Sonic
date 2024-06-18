265b6f485df7cc4dc3367edb4bfeb667dbadbfdf
alex <rusdev.alex@gmail.com>
2024-06-19 00:12:01 +1000

    ./demo files renaming

--- demo/clean.sh
+++ demo/clean.sh
@@ -2,6 +2,6 @@
 cd $(dirname $0)
 
 
-rm -fr opera*.datadir tool.datadir
+rm -fr sonic*.datadir tool.datadir
 rm *.log
-rm ../build/demo_sonicd
+rm ../build/demo_sonicd ../build/sonictool
--- demo/start.sh
+++ demo/start.sh
@@ -13,7 +13,7 @@ popd
 rm -f ./transactions.rlp
 for ((i=0;i<$N;i+=1))
 do
-    DATADIR="${PWD}/opera$i.datadir"
+    DATADIR="${PWD}/sonic$i.datadir"
 
     ACC=$(($i+1))
     if [ ! -e ${DATADIR} ]
@@ -35,7 +35,7 @@ do
 	--http --http.addr="127.0.0.1" --http.port=${RPCP} --http.corsdomain="*" --http.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
 	--ws --ws.addr="127.0.0.1" --ws.port=${WSP} --ws.origins="*" --ws.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
 	--metrics --metrics.addr=127.0.0.1 --metrics.port=$(($RPCP+1100)) \
-	--verbosity=3 --tracing >> opera$i.log 2>&1)&
+	--verbosity=3 --tracing >> sonic$i.log 2>&1)&
 
     echo -e "\tnode$i ok"
 done
