04dfe9643e4924f4d5cc6fd40e798d578e5c542e
JK <jk@fantom.foundation>
2024-10-08 10:51:15 +0200

    Update demo scripts for Sonic

--- demo/start.sh
+++ demo/start.sh
@@ -5,31 +5,36 @@ cd $(dirname $0)
 set -e
 
 echo -e "\nStart $N nodes:\n"
+pushd ..
+make all && mv ./build/sonicd ./build/demo_sonicd
+popd
 
 rm -f ./transactions.rlp
 for ((i=0;i<$N;i+=1))
 do
     DATADIR="${PWD}/sonic$i.datadir"
-    mkdir -p ${DATADIR}
-
     PORT=$(($PORT_BASE+$i))
     RPCP=$(($RPCP_BASE+$i))
     WSP=$(($WSP_BASE+$i))
     ACC=$(($i+1))
 
-    ../build/sonictool \
-  --datadir=${DATADIR} \
-    genesis fake $N
+    if [ ! -e ${DATADIR} ]
+    then
+        echo "Import fake genesis ${ACC}/${N}"
+        mkdir -p ${DATADIR}
+        ../build/sonictool --datadir=${DATADIR} genesis fake ${N} --mode=rpc
+    fi
 
-    (../build/sonicd \
+    (../build/demo_sonicd \
 	--datadir=${DATADIR} \
+	--mode=rpc \
 	--fakenet=${ACC}/$N \
 	--port=${PORT} \
 	--nat extip:127.0.0.1 \
 	--http --http.addr="127.0.0.1" --http.port=${RPCP} --http.corsdomain="*" --http.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
 	--ws --ws.addr="127.0.0.1" --ws.port=${WSP} --ws.origins="*" --ws.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
 	--metrics --metrics.addr=127.0.0.1 --metrics.port=$(($RPCP+1100)) \
-	--verbosity=3 --tracing >> sonicd$i.log 2>&1)&
+	--verbosity=3 >> sonicd$i.log 2>&1)&
 
     echo -e "\tnode$i ok"
 done
