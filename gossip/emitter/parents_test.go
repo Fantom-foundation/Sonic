d28484779416ebff2e979b2d505bbdc2f71e3132
Filip Drobnjakovic <drobnjakovicfilip@gmail.com>
2024-10-21 13:29:15 +0200

    Incorporate style and simplification guidelines
    Co-authored-by: Herbert Jordan <herbert.jordan.jun@gmail.com>

--- gossip/emitter/parents_test.go
+++ gossip/emitter/parents_test.go
@@ -7,14 +7,35 @@ import (
 	"github.com/Fantom-foundation/go-opera/vecmt"
 	"github.com/Fantom-foundation/lachesis-base/emitter/ancestor"
 	"github.com/Fantom-foundation/lachesis-base/hash"
-	"github.com/Fantom-foundation/lachesis-base/inter/dag/tdag"
 	"github.com/Fantom-foundation/lachesis-base/inter/idx"
 	"github.com/Fantom-foundation/lachesis-base/inter/pos"
 	"github.com/Fantom-foundation/lachesis-base/kvdb/memorydb"
 	"github.com/golang/mock/gomock"
 )
 
-func TestChooseParents(t *testing.T) {
+func TestChooseParents_NoParentsForGenesisEvent(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	external := mock.NewMockExternal(ctrl)
+	em := NewEmitter(DefaultConfig(), World{External: external})
+
+	epoch := idx.Epoch(1)
+	validatorId := idx.ValidatorID(1)
+
+	external.EXPECT().GetLastEvent(epoch, validatorId)
+
+	selfParent, parents, ok := em.chooseParents(epoch, validatorId)
+	if selfParent != nil {
+		t.Error("genesis event must not have self parent")
+	}
+	if len(parents) > 0 {
+		t.Error("genesis event must not have any parents")
+	}
+	if !ok {
+		t.Error("genesis parent assignment must always succeed")
+	}
+}
+
+func TestChooseParents_NonGenesisEventMustHaveOneSelfParent(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	external := mock.NewMockExternal(ctrl)
 	em := NewEmitter(DefaultConfig(), World{External: external})
@@ -22,57 +43,29 @@ func TestChooseParents(t *testing.T) {
 	em.payloadIndexer = ancestor.NewPayloadIndexer(3)
 
 	epoch := idx.Epoch(1)
-	nodes := tdag.GenNodes(2)
-	vEmpty, vNonEmpty := nodes[0], nodes[1]
-	selfParentHash := sampleProcessedEvent(1, em.payloadIndexer)
+	validatorId := idx.ValidatorID(1)
 
-	vi := vecmt.NewIndex(nil, vecmt.LiteConfig())
-	vi.Reset(pos.ArrayToValidators(nodes, []pos.Weight{1, 1}), memorydb.New(), nil)
+	validatorIndex := vecmt.NewIndex(nil, vecmt.LiteConfig())
+	validatorIndex.Reset(pos.ArrayToValidators(
+		[]idx.ValidatorID{1, 2},
+		[]pos.Weight{1, 1},
+	), memorydb.New(), nil)
 
-	external.EXPECT().GetLastEvent(epoch, vEmpty).
-		Return(nil).
-		Times(1)
-	external.EXPECT().GetLastEvent(epoch, vNonEmpty).
-		Return(&selfParentHash).
-		Times(1)
-	external.EXPECT().GetHeads(epoch).
-		Return(hash.Events{sampleProcessedEvent(2, em.payloadIndexer), sampleProcessedEvent(3, em.payloadIndexer)}).
-		Times(1)
-	external.EXPECT().DagIndex().
-		Return(vi).
-		Times(1)
+	selfParentHash := hash.Event{1}
 
-	t.Run("emptyChooseParents", func(t *testing.T) {
-		selfParent, parents, ok := em.chooseParents(epoch, vEmpty)
-		if selfParent != nil {
-			t.Error("genesis event must not have self parent")
-		}
-		if len(parents) > 0 {
-			t.Error("genesis event must not have any parents")
-		}
-		if !ok {
-			t.Error("genesis parent assignment must always succeed")
-		}
-	})
+	external.EXPECT().GetLastEvent(epoch, validatorId).Return(&selfParentHash)
+	external.EXPECT().GetHeads(epoch).Return(hash.Events{{2}, {3}})
+	external.EXPECT().DagIndex().Return(validatorIndex)
 
-	t.Run("nonEmptyChooseParents", func(t *testing.T) {
-		selfParent, parents, ok := em.chooseParents(epoch, vNonEmpty)
-		if selfParent == nil {
-			t.Error("non-genesis event must have a self parent")
-		}
-		// strategies sometimes choose the same parent multiple times, test for minimal amount (1 self parent + 1 random/metric)
-		if wantMin, got := 2, len(parents); got < wantMin {
-			t.Errorf("incorrect number of event parents, expected at least: %d, got: %d", wantMin, got)
-		}
-		if !ok {
-			t.Error("parent assignment must succeed when no cheating is detected")
-		}
-	})
-
-}
-
-func sampleProcessedEvent(id uint8, payloadIndexer *ancestor.PayloadIndexer) hash.Event {
-	event := (&tdag.TestEvent{}).Build([24]byte{id})
-	payloadIndexer.ProcessEvent(event, ancestor.Metric(id))
-	return event.ID()
+	selfParent, parents, ok := em.chooseParents(epoch, validatorId)
+	if selfParent == nil {
+		t.Error("non-genesis event must have a self parent")
+	}
+	// strategies sometimes choose the same parent multiple times, test for minimal amount (1 self parent + 1 random/metric)
+	if wantMin, got := 2, len(parents); got < wantMin {
+		t.Errorf("incorrect number of event parents, expected at least: %d, got: %d", wantMin, got)
+	}
+	if !ok {
+		t.Error("parent assignment must succeed when no cheating is detected")
+	}
 }
