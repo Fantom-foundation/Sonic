615764e17d3d4fc5c9d55dd725c2b45023c547d0
facuMH <facumolina15@gmail.com>
2024-11-06 14:58:16 +0100

    remove unnecessary formatting

--- tests/blobbasefee_test.go
+++ tests/blobbasefee_test.go
@@ -19,42 +19,42 @@ import (
 func TestBlobBaseFee_CanReadBlobBaseFeeFromHeadAndBlockAndHistory(t *testing.T) {
 	require := require.New(t)
 	net, err := StartIntegrationTestNet(t.TempDir())
-	require.NoErrorf(err, "Failed to start the fake network: %v", err)
+	require.NoError(err, "Failed to start the fake network: ", err)
 	defer net.Stop()
 
 	// Deploy the blob base fee contract.
 	contract, _, err := DeployContract(net, blobbasefee.DeployBlobbasefee)
-	require.NoErrorf(err, "failed to deploy contract; %v", err)
+	require.NoError(err, "failed to deploy contract; ", err)
 
 	// Collect the current blob base fee from the head state.
 	receipt, err := net.Apply(contract.LogCurrentBlobBaseFee)
-	require.NoErrorf(err, "failed to log current blob base fee; %v", err)
-	require.Equal(len(receipt.Logs), 1, "unexpected number of logs; expected 1, got %d", len(receipt.Logs))
+	require.NoError(err, "failed to log current blob base fee; ", err)
+	require.Equal(len(receipt.Logs), 1, "unexpected number of logs; expected 1, got ", len(receipt.Logs))
 
 	entry, err := contract.ParseCurrentBlobBaseFee(*receipt.Logs[0])
-	require.NoErrorf(err, "failed to parse log; %v", err)
+	require.NoError(err, "failed to parse log; ", err)
 	fromLog := entry.Fee.Uint64()
 
 	// Collect the blob base fee from the block header.
 	client, err := net.GetClient()
-	require.NoErrorf(err, "failed to get client; %v", err)
+	require.NoError(err, "failed to get client; ", err)
 	defer client.Close()
 
 	block, err := client.BlockByNumber(context.Background(), receipt.BlockNumber)
-	require.NoErrorf(err, "failed to get block header; %v", err)
+	require.NoError(err, "failed to get block header; ", err)
 	fromBlock := getBlobBaseFeeFrom(block.Header())
 
 	// Collect the blob base fee from the archive.
 	fromArchive, err := contract.GetBlobBaseFee(&bind.CallOpts{BlockNumber: receipt.BlockNumber})
-	require.NoErrorf(err, "failed to get blob base fee from archive; %v", err)
+	require.NoError(err, "failed to get blob base fee from archive; ", err)
 
 	// call the blob base fee rpc method
 	fromRpc := new(hexutil.Uint64)
 	err = client.Client().Call(&fromRpc, "eth_blobBaseFee")
-	require.NoErrorf(err, "failed to get blob base fee from rpc; %v", err)
+	require.NoError(err, "failed to get blob base fee from rpc; ", err)
 
 	// we check blob base fee is one because it is not implemented yet. TODO issue #147
-	require.Equal(fromLog, uint64(1), "invalid blob base fee from log; %v", fromLog)
+	require.Equal(fromLog, uint64(1), "invalid blob base fee from log; ", fromLog)
 	require.Equal(fromLog, fromArchive.Uint64(), "blob base fee mismatch; from log %v, from archive %v", fromLog, fromArchive)
 	require.Equal(fromLog, fromBlock, "blob base fee mismatch; from log %v, from block %v", fromLog, fromBlock)
 	require.Equal(fromLog, uint64(*fromRpc), "blob base fee mismatch; from log %v, from rpc %v", fromLog, fromRpc)
@@ -72,16 +72,16 @@ func getBlobBaseFeeFrom(header *types.Header) uint64 {
 func TestBlobBaseFee_CanReadBlobGasUsed(t *testing.T) {
 	require := require.New(t)
 	net, err := StartIntegrationTestNet(t.TempDir())
-	require.NoErrorf(err, "Failed to start the fake network: %v", err)
+	require.NoError(err, "Failed to start the fake network: ", err)
 	defer net.Stop()
 
 	client, err := net.GetClient()
-	require.NoErrorf(err, "failed to get client; %v", err)
+	require.NoError(err, "failed to get client; ", err)
 	defer client.Close()
 
 	// Get blob gas used from the block header.
 	block, err := client.BlockByNumber(context.Background(), big.NewInt(0))
-	require.NoErrorf(err, "failed to get block header; %v", err)
+	require.NoError(err, "failed to get block header; ", err)
 	require.Empty(*block.BlobGasUsed(), "unexpected value in blob gas used")
 
 	// check value for blob gas used is rlp endoded and decoded
@@ -91,10 +91,10 @@ func TestBlobBaseFee_CanReadBlobGasUsed(t *testing.T) {
 
 	buffer := bytes.NewBuffer(make([]byte, 0))
 	err = newBlock.EncodeRLP(buffer)
-	require.NoErrorf(err, "failed to encode block header; %v", err)
+	require.NoError(err, "failed to encode block header; ", err)
 	stream := rlp.NewStream(buffer, 0)
 	err = newBlock.DecodeRLP(stream)
-	require.NoErrorf(err, "failed to decode block header; %v", err)
+	require.NoError(err, "failed to decode block header; ", err)
 
 	// check blob gas used and excess blob gas are zero
 	require.Empty(*block.BlobGasUsed(), "unexpected blob gas used value")
