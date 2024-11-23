95a05a7619764f61784f41e3d688e9ff0a0e9cd4
Herbert Jordan <herbert@swrnd.ch>
2024-11-23 16:27:03 +0100

    Add unit tests for connection advisor

--- gossip/topology/connection_advisor.go
+++ gossip/topology/connection_advisor.go
@@ -23,79 +23,102 @@ type ConnectionAdvisor interface {
 }
 
 func NewConnectionAdvisor(localId enode.ID) ConnectionAdvisor {
-	return &connectionAdvisor{
-		neighborhood: make(map[enode.ID]neighborhoodEntry),
-		localId:      localId,
+	return newConnectionAdvisor[enode.ID, *enode.Node](
+		localId, 60*time.Second, func(n *enode.Node) enode.ID { return n.ID() },
+	)
+}
+
+func newConnectionAdvisor[I comparable, R any](
+	localId I,
+	maxPeerInfoAge time.Duration,
+	getId func(R) I,
+) *connectionAdvisor[I, R] {
+	return &connectionAdvisor[I, R]{
+		neighborhood:   make(map[I]neighborhoodEntry[R]),
+		localId:        localId,
+		getId:          getId,
+		maxPeerInfoAge: maxPeerInfoAge,
 	}
 }
 
-// maxPeerInfoAge is the maximum age of peer information that is considered
-// when making suggestions on adding or removing peers. Older information
-// is discarded.
-const maxPeerInfoAge = 60 * time.Second
-
-type connectionAdvisor struct {
+// connectionAdvisor is a ConnectionAdvisor implementation that suggests new peers
+// to connect to and redundant peers to disconnect from based on a simple heuristic.
+// The implementation is generic to simplify testing. The type parameter I is the
+// type of the peer ID and R is the type of the peer reference required to establish
+// a connection.
+type connectionAdvisor[I comparable, R any] struct {
 	mu sync.Mutex
 
 	// Keep track of the neighbors of each peer.
-	neighborhood map[enode.ID]neighborhoodEntry
+	neighborhood map[I]neighborhoodEntry[R]
 
 	// The ID of the local node.
-	localId enode.ID
+	localId I
+
+	// maxPeerInfoAge is the maximum age of peer information that is considered
+	// when making suggestions on adding or removing peers. Older information
+	// is discarded.
+	maxPeerInfoAge time.Duration
+
+	// getId returns the ID of a stored peer reference
+	getId func(R) I
 }
 
-type neighborhoodEntry struct {
-	peers []*enode.Node
+type neighborhoodEntry[T any] struct {
+	peers []T
 	time  time.Time
 }
 
-func (c *connectionAdvisor) GetNewPeerSuggestion() *enode.Node {
+func (c *connectionAdvisor[I, T]) GetNewPeerSuggestion() T {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	// Search for a peer of a peer that is not already connected to the local node.
 	now := time.Now()
 	for peer, entry := range c.neighborhood {
-		if now.Sub(entry.time) > maxPeerInfoAge {
+		if now.Sub(entry.time) > c.maxPeerInfoAge {
 			delete(c.neighborhood, peer)
 			continue
 		}
 		for _, peer := range entry.peers {
-			if peer.ID() == c.localId {
+			peerId := c.getId(peer)
+			if peerId == c.localId {
 				continue
 			}
-			if _, found := c.neighborhood[peer.ID()]; !found {
+			if _, found := c.neighborhood[peerId]; !found {
 				return peer
 			}
 		}
 	}
 
-	return nil
+	var zero T
+	return zero
 }
 
-func (c *connectionAdvisor) GetRedundantPeerSuggestion() *enode.ID {
+func (c *connectionAdvisor[I, T]) GetRedundantPeerSuggestion() *I {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	// Count the number of indirect connections to all peers and
 	// recommend the one that has the most indirect connections.
-	count := map[enode.ID]int{}
+	count := map[I]int{}
 	now := time.Now()
 	for peer, entry := range c.neighborhood {
-		if now.Sub(entry.time) > maxPeerInfoAge {
+		if now.Sub(entry.time) > c.maxPeerInfoAge {
 			delete(c.neighborhood, peer)
 			continue
 		}
 		for _, peer := range entry.peers {
-			if _, found := c.neighborhood[peer.ID()]; found {
-				count[peer.ID()]++
+			peerId := c.getId(peer)
+			if _, found := c.neighborhood[peerId]; found {
+				count[peerId]++
 			}
 		}
 	}
 	delete(count, c.localId)
 
 	var maxCount int
-	var maxPeer enode.ID
+	var maxPeer I
 	for peer, c := range count {
 		if c > maxCount {
 			maxCount = c
@@ -109,10 +132,10 @@ func (c *connectionAdvisor) GetRedundantPeerSuggestion() *enode.ID {
 	return &maxPeer
 }
 
-func (c *connectionAdvisor) UpdatePeers(peer enode.ID, peers []*enode.Node) {
+func (c *connectionAdvisor[I, T]) UpdatePeers(peer I, peers []T) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.neighborhood[peer] = neighborhoodEntry{
+	c.neighborhood[peer] = neighborhoodEntry[T]{
 		peers: peers,
 		time:  time.Now(),
 	}
--- gossip/topology/connection_advisor_test.go
+++ gossip/topology/connection_advisor_test.go
@@ -1,132 +1,291 @@
 package topology
 
 import (
-	"encoding/binary"
-	"fmt"
 	"slices"
-	"strings"
 	"testing"
-
-	"github.com/ethereum/go-ethereum/p2p/enode"
-	"github.com/ethereum/go-ethereum/p2p/enr"
+	"time"
 )
 
-func TestConnectionAdvisor_LinearTopologyTurnsIntoMesh(t *testing.T) {
+func TestConnectionAdvisor_SuggestToConnectToPeerOfPeer(t *testing.T) {
+	// Connection pattern: 1 <-> 2 <-> 3
+	advisor := newAdvisor(1)
+	advisor.UpdatePeers(2, []int{1, 3})
+	if want, got := 3, advisor.GetNewPeerSuggestion(); want != got {
+		t.Fatalf("unexpected suggestion: want=%d got=%d", want, got)
+	}
+}
 
+func TestConnectionAdvisor_SuggestsNothingIfThereIsNoPeerOfAPeer(t *testing.T) {
+	// Connection pattern: 1
+	advisor := newAdvisor(1)
+	if want, got := 0, advisor.GetNewPeerSuggestion(); want != got {
+		t.Fatalf("unexpected suggestion: want=%d got=%d", want, got)
+	}
+
+	// Connection pattern: 1 <-> 2
+	advisor.UpdatePeers(2, []int{1})
+	if want, got := 0, advisor.GetNewPeerSuggestion(); want != got {
+		t.Fatalf("unexpected suggestion: want=%d got=%d", want, got)
+	}
+}
+
+func TestConnectionAdvisor_SuggestToRemoveRedundantConnection(t *testing.T) {
+	// Connection pattern: (1,2), (1,3), (1,4), (2,3), (3,4)
+	// The connection (1,3) is redundant.
+	advisor := newAdvisor(1)
+	advisor.UpdatePeers(2, []int{1, 3})
+	advisor.UpdatePeers(3, []int{1})
+	advisor.UpdatePeers(4, []int{1, 3})
+	if want, got := 3, advisor.GetRedundantPeerSuggestion(); got == nil || want != *got {
+		t.Fatalf("unexpected suggestion: want=%d got=%v", want, got)
+	}
+}
+
+func TestConnectionAdvisor_DoesNotSuggestToRemovePeerIfNoneIsRedundant(t *testing.T) {
+	// Connection pattern: 1
+	advisor := newAdvisor(1)
+	if got := advisor.GetRedundantPeerSuggestion(); got != nil {
+		t.Fatalf("unexpected suggestion: got=%v", got)
+	}
+
+	// Connection pattern: 1 <-> 2
+	advisor.UpdatePeers(2, []int{1})
+	if got := advisor.GetRedundantPeerSuggestion(); got != nil {
+		t.Fatalf("unexpected suggestion: got=%v", got)
+	}
+
+	// Connection pattern: 1 <-> 2, 1 <-> 3
+	advisor.UpdatePeers(3, []int{1})
+	if got := advisor.GetRedundantPeerSuggestion(); got != nil {
+		t.Fatalf("unexpected suggestion: got=%v", got)
+	}
+}
+
+func TestConnectionAdvisor_NewPeerSuggestionPrunesOutdatedPeerInformation(t *testing.T) {
+	// Connection pattern: 1 <-> 2 <-> 3
+	advisor := newAdvisor(1)
+	advisor.UpdatePeers(2, []int{1, 3})
+	if want, got := 3, advisor.GetNewPeerSuggestion(); want != got {
+		t.Fatalf("unexpected suggestion: want=%d got=%d", want, got)
+	}
+
+	if len(advisor.neighborhood) == 0 {
+		t.Fatalf("unexpected neighborhood: got=%v", advisor.neighborhood)
+	}
+
+	// Set the maximum age of peer information to 0.
+	advisor.maxPeerInfoAge = 0
+
+	// All knowledge about the network should have been forgotten.
+	if want, got := 0, advisor.GetNewPeerSuggestion(); want != got {
+		t.Fatalf("unexpected suggestion: want=%d got=%d", want, got)
+	}
+
+	if len(advisor.neighborhood) != 0 {
+		t.Fatalf("unexpected neighborhood: got=%v", advisor.neighborhood)
+	}
+}
+
+func TestConnectionAdvisor_RedundantPeerSuggestionPrunesOutdatedPeerInformation(t *testing.T) {
+	advisor := newAdvisor(1)
+	advisor.UpdatePeers(2, []int{1, 3})
+	advisor.UpdatePeers(3, []int{1})
+	advisor.UpdatePeers(4, []int{1, 3})
+	if got := advisor.GetRedundantPeerSuggestion(); got == nil {
+		t.Fatalf("unexpected suggestion: got=%v", got)
+	}
+
+	if len(advisor.neighborhood) == 0 {
+		t.Fatalf("unexpected neighborhood: got=%v", advisor.neighborhood)
+	}
+
+	// Set the maximum age of peer information to 0.
+	advisor.maxPeerInfoAge = 0
+
+	// All knowledge about the network should have been forgotten.
+	if got := advisor.GetRedundantPeerSuggestion(); got != nil {
+		t.Fatalf("unexpected suggestion: got=%v", got)
+	}
+
+	if len(advisor.neighborhood) != 0 {
+		t.Fatalf("unexpected neighborhood: got=%v", advisor.neighborhood)
+	}
+}
+
+func TestConnectionAdvisor_LinearTopologyTurnsIntoMesh(t *testing.T) {
 	const N = 10
 
-	nodes := []node{}
-	for i := range N {
-		nodes = append(nodes, node{
-			id: createENode(i),
-		})
-	}
-
 	// Create a linear topology
-	for i := range nodes {
-		if i > 0 {
-			nodes[i].peers = append(nodes[i].peers, nodes[i-1].id)
-		}
-		if i < len(nodes)-1 {
-			nodes[i].peers = append(nodes[i].peers, nodes[i+1].id)
-		}
+	net := newNetwork(N)
+	for i := 0; i < N-1; i++ {
+		net.connect(i, i+1)
 	}
 
-	print := func() {
-		for i := range nodes {
-			fmt.Printf("Node %d has %d peers: ", i, len(nodes[i].peers))
-			ids := []int{}
-			for _, peer := range nodes[i].peers {
-				ids = append(ids, toId(peer))
-			}
-			slices.Sort(ids)
-			idStrings := []string{}
-			for _, id := range ids {
-				idStrings = append(idStrings, fmt.Sprintf("%d", id))
-			}
-			fmt.Printf("%s\n", strings.Join(idStrings, ", "))
-		}
-		fmt.Println()
+	if want, got := N-1, net.getDiameter(); want != got {
+		t.Fatalf("unexpected diameter: want=%d got=%d", want, got)
 	}
 
-	print()
-
-	for range 2 * N {
-		newPeers := make([]*enode.Node, len(nodes))
-		remPeers := make([]*enode.ID, len(nodes))
-		for i, node := range nodes {
-			self := node.id.ID()
-			advisor := NewConnectionAdvisor(self)
+	// Add connections in turns as suggested by the connection advisor.
+	last := net.getDiameter()
+	for range N {
+		newPeers := make([]int, N)
+		for i, node := range net.nodes {
+			advisor := newAdvisor(node.id)
 			for _, peer := range node.peers {
-				advisor.UpdatePeers(peer.ID(), nodes[toId(peer)].peers)
+				advisor.UpdatePeers(peer, net.nodes[peer].peers)
 			}
-			newPeer := advisor.GetNewPeerSuggestion()
-			if newPeer != nil {
-				fmt.Printf("Node %d should connect to %d\n", toId(node.id), toId(newPeer))
-			} else {
-				fmt.Printf("Node %d should not connect to any new peers\n", toId(node.id))
-			}
-			newPeers[i] = newPeer
-
-			removePeer := advisor.GetRedundantPeerSuggestion()
-			if removePeer != nil {
-				fmt.Printf("Node %d should remove %d\n", toId(node.id), toId2(*removePeer))
-			} else {
-				fmt.Printf("Node %d should not remove any peers\n", toId(node.id))
-			}
-			remPeers[i] = removePeer
+			newPeers[i] = advisor.GetNewPeerSuggestion()
 		}
 
 		for i, newPeer := range newPeers {
-			if newPeer == nil {
+			if newPeer == 0 {
 				continue
 			}
-			if slices.Contains(nodes[i].peers, newPeer) {
-				continue
-			}
-			nodes[i].peers = append(nodes[i].peers, newPeer)
+			net.connect(i, newPeer)
 		}
 
-		for i, remPeer := range remPeers {
-			if remPeer == nil {
-				continue
-			}
-			if len(nodes[i].peers) > 4 {
-				nodes[i].peers = slices.DeleteFunc(nodes[i].peers, func(peer *enode.Node) bool {
-					return peer.ID() == *remPeer
-				})
-			}
+		diameter := net.getDiameter()
+		if diameter > last {
+			t.Errorf("diameter increased: had=%d new=%d", last, diameter)
 		}
-
-		fmt.Println()
-
-		print()
+		last = diameter
 	}
 
-	t.Fail()
+	// The linear topology should have turned into a mesh.
+	if want, got := 1, net.getDiameter(); want != got {
+		t.Errorf("unexpected diameter: want=%d got=%d", want, got)
+	}
+}
+
+func TestConnectionAdvisor_GradualRefinementCanReachOptimum(t *testing.T) {
+	const N = 16 // Number of nodes
+	const C = 4  // Maximum number of connections per node; enough to reach 16 nodes in 2 hops.
+
+	// Create a linear topology
+	net := newNetwork(N)
+	for i := 0; i < N-1; i++ {
+		net.connect(i, i+1)
+	}
+
+	if want, got := N-1, net.getDiameter(); want != got {
+		t.Fatalf("unexpected diameter: want=%d got=%d", want, got)
+	}
+
+	// Updating connections should gradually turn the linear topology into a mesh.
+	for range 10 * N {
+		// Remove suggested redundant peers.
+		for i, node := range net.nodes {
+			if len(node.peers) <= C {
+				continue
+			}
+			advisor := newAdvisor(node.id)
+			for _, peer := range node.peers {
+				advisor.UpdatePeers(peer, net.nodes[peer].peers)
+			}
+			if toRemove := advisor.GetRedundantPeerSuggestion(); toRemove != nil {
+				net.disconnect(i, *toRemove)
+			}
+		}
+
+		// Add new connections.
+		newPeers := make([]int, N)
+		for i, node := range net.nodes {
+			advisor := newAdvisor(node.id)
+			for _, peer := range node.peers {
+				advisor.UpdatePeers(peer, net.nodes[peer].peers)
+			}
+			newPeers[i] = advisor.GetNewPeerSuggestion()
+		}
+
+		for i, newPeer := range newPeers {
+			if newPeer == 0 {
+				continue
+			}
+			net.connect(i, newPeer)
+		}
+
+		diameter := net.getDiameter()
+		if diameter == 2 {
+			break
+		}
+	}
+
+	// Connections should be effectively used to reach all nodes in 2 hops.
+	if want, got := 2, net.getDiameter(); want != got {
+		t.Errorf("unexpected diameter: want=%d got=%d", want, got)
+	}
 }
 
 type node struct {
-	id    *enode.Node
-	peers []*enode.Node
+	id    int
+	peers []int
 }
 
-func createENode(i int) *enode.Node {
-	id := enode.ID{}
-	binary.BigEndian.PutUint64(id[24:], uint64(i))
-	r := enr.Record{}
-	r.Set(enr.WithEntry("nulladdr", &id))
-	node, err := enode.New(enode.NullID{}, &r)
-	if err != nil {
-		panic(err)
+type network struct {
+	nodes []node
+}
+
+func newNetwork(n int) *network {
+	nodes := make([]node, n)
+	for i := range nodes {
+		nodes[i].id = i
 	}
-	return node
+	return &network{nodes: nodes}
 }
 
-func toId(node *enode.Node) int {
-	return toId2(node.ID())
+func (n *network) connect(i, j int) {
+	if !slices.Contains(n.nodes[i].peers, j) {
+		n.nodes[i].peers = append(n.nodes[i].peers, j)
+		n.nodes[j].peers = append(n.nodes[j].peers, i)
+	}
 }
 
-func toId2(node enode.ID) int {
-	return int(binary.BigEndian.Uint64(node[24:]))
+func (n *network) disconnect(i, j int) {
+	n.nodes[i].peers = slices.DeleteFunc(n.nodes[i].peers, func(peer int) bool { return peer == j })
+	n.nodes[j].peers = slices.DeleteFunc(n.nodes[j].peers, func(peer int) bool { return peer == i })
+}
+
+func (n *network) getDiameter() int {
+	// Floyd-Warshall algorithm
+	const INF = 1e9
+	dist := make([][]int, len(n.nodes))
+	for i := range dist {
+		dist[i] = make([]int, len(n.nodes))
+		for j := range dist[i] {
+			dist[i][j] = INF
+		}
+		dist[i][i] = 0
+	}
+
+	for i, node := range n.nodes {
+		for _, peer := range node.peers {
+			dist[i][peer] = 1
+			dist[peer][i] = 1
+		}
+	}
+
+	for k := range n.nodes {
+		for i := range n.nodes {
+			for j := range n.nodes {
+				if dist[i][j] > dist[i][k]+dist[k][j] {
+					dist[i][j] = dist[i][k] + dist[k][j]
+				}
+			}
+		}
+	}
+
+	diameter := 0
+	for i := range n.nodes {
+		for j := range n.nodes {
+			if dist[i][j] > diameter {
+				diameter = dist[i][j]
+			}
+		}
+	}
+
+	return diameter
+}
+
+func newAdvisor(localId int) *connectionAdvisor[int, int] {
+	return newConnectionAdvisor(localId, time.Second, func(i int) int { return i })
 }
