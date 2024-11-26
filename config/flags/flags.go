40b7bbc173031da118c30b65a0195ce72778a222
Petr Hanzl <petrhanzl33@gmail.com>
2024-11-26 13:08:13 +0100

    Use better words in documentation

--- config/flags/flags.go
+++ config/flags/flags.go
@@ -355,16 +355,14 @@ var (
 		Name: "statedb.livecache",
 		Usage: fmt.Sprintf("Size of live db cache in bytes. Leaving this blank (which is generally recommended),"+
 			"or setting this to <1 will automatically allocate cache size depending on how much cache you use with %s."+
-			"Setting this value to <=2000 will result in cache capacity of 2KB (which is the lowest value that Carmen allows)",
-			CacheFlag.Name),
+			"Setting this value to <=2000 will result in pre-confugired cache capacity of 2KB", CacheFlag.Name),
 		Value: 0,
 	}
 	ArchiveCacheFlag = cli.IntFlag{
 		Name: "statedb.archivecache",
 		Usage: fmt.Sprintf("Size of archive cache in bytes. Leaving this blank (which is generally recommended),"+
 			"or setting this to <1 will automatically allocate cache size depending on how much cache you use with %s."+
-			"Setting this value to <=2000 will result in cache capacity of 2KB (which is the lowest value that Carmen allows)",
-			CacheFlag.Name),
+			"Setting this value to <=2000 will result in pre-confugired cache capacity of 2KB", CacheFlag.Name),
 		Value: 0,
 	}
 )
