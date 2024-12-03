dfa59271f33dbd1920954d017fda1d0ffa53716b
Petr Hanzl <petrhanzl33@gmail.com>
2024-12-03 10:08:49 +0100

    Correct integration test.

--- tests/get_account_test.go
+++ tests/get_account_test.go
@@ -4,52 +4,52 @@ import (
 	"github.com/Fantom-foundation/go-opera/ethapi"
 	"github.com/Fantom-foundation/go-opera/tests/contracts/transientstorage"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/holiman/uint256"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestGetAccount(t *testing.T) {
 	net, err := StartIntegrationTestNet(t.TempDir())
-	if err != nil {
-		t.Fatalf("Failed to start the fake network: %v", err)
-	}
+	require.NoError(t, err, "failed to start the fake network")
 	defer net.Stop()
 
 	// Deploy the transient storage contract
 	_, deployReceipt, err := DeployContract(net, transientstorage.DeployTransientstorage)
-	if err != nil {
-		t.Fatalf("failed to deploy contract; %v", err)
-	}
+	require.NoError(t, err, "failed to deploy contract")
 
 	addr := deployReceipt.ContractAddress
 
 	c, err := net.GetClient()
-	if err != nil {
-		t.Fatalf("failed to get client; %v", err)
-	}
+	require.NoError(t, err, "failed to get client")
+	defer c.Close()
 
 	rpcClient := c.Client()
+	defer rpcClient.Close()
 
 	var res ethapi.GetAccountResult
 	err = rpcClient.Call(&res, "eth_getAccount", addr, rpc.LatestBlockNumber)
-	if err != nil {
-		t.Fatalf("failed to get account; %v", err)
-	}
+	require.NoError(t, err, "failed to call get account")
 
-	if res.CodeHash == (common.Hash{}) {
-		t.Error("code hash should not be empty")
+	// Extract proof to find actual StorageHash(Root), Nonce, Balance and CodeHash
+	var proofRes struct {
+		StorageHash common.Hash
+		Nonce       hexutil.Uint64
+		Balance     *hexutil.U256
+		CodeHash    common.Hash
 	}
+	err = rpcClient.Call(
+		&proofRes,
+		"eth_getProof",
+		addr,
+		nil,
+		rpc.LatestBlockNumber,
+	)
+	require.NoError(t, err, "failed call to get proof")
 
-	if res.StorageRoot == (common.Hash{}) {
-		t.Error("storage root should not be empty")
-	}
-
-	if got, want := (*uint256.Int)(res.Balance).Uint64(), uint64(0); got != want {
-		t.Errorf("balance not as expected, got: %d want: %d", got, want)
-	}
-
-	if res.Nonce < 1 {
-		t.Errorf("account nonce is expected to by at least 1")
-	}
+	require.Equal(t, proofRes.CodeHash, res.CodeHash)
+	require.Equal(t, proofRes.StorageHash, res.StorageRoot)
+	require.Equal(t, proofRes.Balance, res.Balance)
+	require.Equal(t, proofRes.Nonce, res.Nonce)
 }
