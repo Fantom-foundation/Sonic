58528a81c29143c6a3eb35ad3898bce25570e4a2
facuMH <facumolina15@gmail.com>
2024-12-16 10:38:42 +0100

    first uses of caution

--- cmd/sonictool/app/export_genesis.go
+++ cmd/sonictool/app/export_genesis.go
@@ -3,20 +3,22 @@ package app
 import (
 	"context"
 	"fmt"
-	"github.com/Fantom-foundation/go-opera/cmd/sonictool/db"
-	"github.com/Fantom-foundation/go-opera/cmd/sonictool/genesis"
-	"github.com/Fantom-foundation/go-opera/config/flags"
-	"github.com/Fantom-foundation/go-opera/integration"
-	"github.com/syndtr/goleveldb/leveldb/opt"
-	"gopkg.in/urfave/cli.v1"
 	"os"
 	"os/signal"
 	"path"
 	"path/filepath"
 	"syscall"
+
+	"github.com/Fantom-foundation/go-opera/cmd/sonictool/db"
+	"github.com/Fantom-foundation/go-opera/cmd/sonictool/genesis"
+	"github.com/Fantom-foundation/go-opera/config/flags"
+	"github.com/Fantom-foundation/go-opera/integration"
+	"github.com/Fantom-foundation/go-opera/utils/caution"
+	"github.com/syndtr/goleveldb/leveldb/opt"
+	"gopkg.in/urfave/cli.v1"
 )
 
-func exportGenesis(ctx *cli.Context) error {
+func exportGenesis(ctx *cli.Context) (err error) {
 	dataDir := ctx.GlobalString(flags.DataDirFlag.Name)
 	if dataDir == "" {
 		return fmt.Errorf("--%s need to be set", flags.DataDirFlag.Name)
@@ -45,7 +47,7 @@ func exportGenesis(ctx *cli.Context) error {
 	if err != nil {
 		return fmt.Errorf("failed to make DB producer: %v", err)
 	}
-	defer dbs.Close()
+	defer caution.CloseAndReportError(&err, dbs, "failed to close DB producer")
 
 	gdb, err := db.MakeGossipDb(db.GossipDbParameters{
 		Dbs:           dbs,
@@ -58,17 +60,18 @@ func exportGenesis(ctx *cli.Context) error {
 	if err != nil {
 		return err
 	}
-	defer gdb.Close()
+	defer caution.CloseAndReportError(&err, gdb, "failed to close Gossip DB")
 
-	fh, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
+	fileHandler, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, os.ModePerm)
 	if err != nil {
 		return err
 	}
-	defer fh.Close()
+	defer caution.CloseAndReportError(&err, fileHandler, fmt.Sprintf("failed to close file %v", fileName))
 
 	tmpPath := path.Join(dataDir, "tmp-genesis-export")
 	_ = os.RemoveAll(tmpPath)
-	defer os.RemoveAll(tmpPath)
+	defer caution.ExecuteAndReportError(&err, func() error { return os.RemoveAll(tmpPath) },
+		"failed to remove tmp genesis export dir")
 
-	return genesis.ExportGenesis(cancelCtx, gdb, !forValidatorMode, fh, tmpPath)
+	return genesis.ExportGenesis(cancelCtx, gdb, !forValidatorMode, fileHandler, tmpPath)
 }
