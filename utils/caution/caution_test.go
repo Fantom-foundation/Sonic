1f35af9ebf751b82bcc2fa6bc823e21483842cfe
facuMH <facumolina15@gmail.com>
2024-12-16 10:18:58 +0100

    refactor and add documentation

--- utils/caution/caution_test.go
+++ utils/caution/caution_test.go
@@ -1,52 +1,68 @@
 package caution
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-func TestExecuteAndReportError_DoesNothingWhenInputErrorIsNotNil(t *testing.T) {
-	err := fmt.Errorf("someError")
-	ExecuteAndReportError(&err, func() error {
-		panic("mypanic")
-	}, "mymessage")
-	if err.Error() != "someError" {
-		t.Errorf("error not reported, got %v", err)
-	}
-}
-
-func TestExecuteAndReportError_ExecutesFunAndReportsError(t *testing.T) {
-	var err error
-	ExecuteAndReportError(&err, func() error {
-		return fmt.Errorf("someError")
-	}, "message")
-	if errors.Is(err, fmt.Errorf("message: someError")) {
-		t.Errorf("unexpected error, got %v", err)
-	}
-}
-
-func TestExecuteAndReportError_ExecutesFunNothingToReport(t *testing.T) {
+func TestExecuteAndReportError_ExecutesAndReturnsError(t *testing.T) {
 	var err error
 	ExecuteAndReportError(&err, func() error {
 		return nil
 	}, "message")
-	if err != nil {
-		t.Errorf("unexpected error, got %v", err)
-	}
+	require.NoError(t, err)
+	someError := fmt.Errorf("someError")
+	ExecuteAndReportError(&err, func() error {
+		return someError
+	}, "message")
+	require.ErrorIs(t, err, someError)
+}
+
+func TestExecuteAndReportError_ExecutesAndCombinesErrors(t *testing.T) {
+	firstError := fmt.Errorf("firstError")
+	err := firstError
+	ExecuteAndReportError(&err, func() error {
+		return nil
+	}, "message")
+	require.ErrorIs(t, err, firstError)
+
+	ExecuteAndReportError(&err, func() error {
+		return fmt.Errorf("secondError")
+	}, "message")
+	require.ErrorContains(t, err, "firstError")
+	require.ErrorContains(t, err, "secondError")
+}
+
+type closeMe struct {
+	err error
+}
+
+func (c *closeMe) Close() error {
+	return c.err
 }
 
 func TestCloseAndReportError_(t *testing.T) {
-	tmpfile := filepath.Join(t.TempDir(), "file")
-	file, err := os.Create(tmpfile)
-	if err != nil {
-		t.Fatal(err)
+	file := &closeMe{}
+	var err error
+	CloseAndReportError(&err, file, "message")
+	require.NoError(t, err)
+
+	file.err = fmt.Errorf("someError")
+	CloseAndReportError(&err, file, "message")
+	require.ErrorContains(t, err, "message: someError")
+}
+
+func TestCloseAndReportError_UsagePatternPropagatesError(t *testing.T) {
+	expectedError := fmt.Errorf("someError")
+
+	testFun := func() (outErr error) {
+		file := &closeMe{err: expectedError}
+		defer CloseAndReportError(&outErr, file, "message")
+		return
 	}
 
-	CloseAndReportError(&err, file, "message")
-	if _, err := file.Read([]byte{0}); err == nil {
-		t.Errorf("file not closed, %v", err)
-	}
+	gotError := testFun()
+	require.ErrorIs(t, gotError, expectedError)
 }
